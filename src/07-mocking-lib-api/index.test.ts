f1bf605c2b36d25a85be36ef2a43d0396ed3bdc9
Zahar Papenko <zakharpapenko@gmail.com>
2025-05-12 12:57:00 +0300

    feat: 07-mocking lib

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,29 @@
 // Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios, { AxiosInstance } from 'axios';
+import { throttledGetDataFromApi } from './index';
 
 describe('throttledGetDataFromApi', () => {
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const spy = jest.spyOn(axios, 'create');
+    const way = '/users';
+    jest.useFakeTimers();
+    throttledGetDataFromApi(way);
+    jest.runAllTimers();
+    expect(spy).toHaveBeenCalledWith({
+      baseURL: 'https://jsonplaceholder.typicode.com',
+    });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const getSpy = jest.fn();
+    jest.spyOn(axios, 'create').mockReturnValue({
+      get: getSpy,
+    } as unknown as AxiosInstance);
+    jest.useFakeTimers();
+    throttledGetDataFromApi('/users');
+    jest.runAllTimers();
+    expect(getSpy).toHaveBeenCalledWith('/users');
   });
 
-  test('should return response data', async () => {
-    // Write your test here
-  });
+  test('should return response data', async () => {});
 });
